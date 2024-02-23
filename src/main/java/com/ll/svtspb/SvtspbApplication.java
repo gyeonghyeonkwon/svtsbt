05925785d7fca7bb9c67bac787aae6e40ad854e8
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-02-23 15:40:52 +0900

    스벨트 킷 설치

---
+++ src/main/java/com/ll/svtspb/SvtspbApplication.java
@@ -0,0 +1,13 @@
+package com.ll.svtspb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtspbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvtspbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/svtspb/SvtspbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.svtspb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvtspbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
