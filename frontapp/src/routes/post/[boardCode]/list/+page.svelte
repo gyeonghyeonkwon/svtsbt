f2b5a0f90b0ffa4294dceaf3b19422d47aed4839
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-02-23 22:46:32 +0900

    스벨트 동적라우팅

---
+++ frontapp/src/routes/post/[boardCode]/list/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {page} from '$app/stores'
+    // const boardCode = $page.params['boardCode'];
+
+  const boardCode =  $page.params['boardCode']
+
+
+    const boardNames = {
+
+        notice: '공지사항' ,
+        free1: '잡담' ,
+        free2: '방명록'
+    };
+
+    const boardName = (boardNames[boardCode] || '') + ' 게시판'.trim();
+</script>
+
+
+<h1>{boardName}</h1>
